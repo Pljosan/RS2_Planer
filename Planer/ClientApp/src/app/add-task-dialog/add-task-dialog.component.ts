4248a0c6d80c11ba83eb8dfdd712de8a46412baa
Vesna Katanic <vesna.katanic@gmail.com>
2018-11-25 14:58:54 +0100

    calendar, add task dialog start

---
+++ Planer/ClientApp/src/app/add-task-dialog/add-task-dialog.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import {FormControl, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-add-task-dialog',
+  templateUrl: './add-task-dialog.component.html',
+  styleUrls: ['./add-task-dialog.component.scss']
+})
+export class AddTaskDialogComponent implements OnInit {
+
+  task = {
+    name: 'Test',
+    date: '12/11/2018',
+    time: '10:10:10',
+    userId: 4,
+    desc: 'tra la la al'
+  };
+
+  form: FormGroup;
+
+  xxx = moment().format('YYYY-MM-DD');
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.xxx);
+    this.form = new FormGroup({
+      name: new FormControl('ttt'),
+      date: new FormControl(this.xxx)
+    })
+  }
+
+}
