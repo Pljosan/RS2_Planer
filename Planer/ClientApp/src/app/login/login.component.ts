31d842d7c294c40dc7768bafb6215bac43f3f444
Vesna Katanic <vesna.katanic@gmail.com>
2018-11-25 15:11:43 +0100

    type error fix

--- Planer/ClientApp/src/app/login/login.component.ts
+++ Planer/ClientApp/src/app/login/login.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {MatDialog} from '@angular/material'
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router) { }
-username: string;
-password: string;
+  constructor(private router: Router) {
+  }
+
+  username: string;
+  password: string;
+
   ngOnInit() {
   }
-  login() : void {
-    if(this.username == 'admin' && this.password == 'admin'){
-     this.router.navigate(["users"]);
-    }else {
+
+  login(): void {
+    if (this.username == 'admin' && this.password == 'admin') {
+      this.router.navigate(["users"]);
+    } else {
       alert("Invalid credentials");
     }
   }
-  }
+
 }
