f7e3bdf2b84cabcd4c60d170070da78306f4145a
Pljosan <ana.bajic13@gmail.com>
2018-11-29 23:09:39 +0100

    upload file to desired folder

--- Planer/ClientApp/src/app/fetch-folders/fetch-folders.component.ts
+++ Planer/ClientApp/src/app/fetch-folders/fetch-folders.component.ts
@@ -1,12 +1,15 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {MatDialog, MatDialogConfig} from "@angular/material";
+import { FileUploadComponent } from '../file-upload/file-upload.component';
+
 
 
 @Component({
   selector: 'app-fetch-folders',
   templateUrl: './fetch-folders.component.html'
 })
-export class FetchFoldersComponent {
+export class FetchFoldersComponent implements OnInit {
 
     public foldersMap: Map<string, Array<string>>;
 
@@ -15,7 +18,7 @@ export class FetchFoldersComponent {
     public files: Array<string>;
     public baseUrl: string;
     
-    constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) { 
+    constructor(private http: HttpClient, private dialog: MatDialog, @Inject('BASE_URL') baseUrl: string) { 
         this.baseUrl = baseUrl;
 
         this.foldersMap = new Map<string, Array<string>>();
@@ -26,6 +29,10 @@ export class FetchFoldersComponent {
         http.post<Folder>(baseUrl + 'api/Folder/getRootFolder', user).subscribe(result => {
             this.setContentsAfterPost(result, false);
           }, error => console.error(error));
+    }
+    
+    ngOnInit() {
+
     }
 
     setContentsAfterPost(result: Folder, back: boolean) {
@@ -66,6 +73,60 @@ export class FetchFoldersComponent {
             }
         });
     }
+
+    openFileUploadDialog(path: string) {
+
+        const dialogConfig = new MatDialogConfig();
+
+        dialogConfig.disableClose = true;
+        dialogConfig.autoFocus = true;
+
+        const dialogRef = this.dialog.open(FileUploadComponent, dialogConfig);
+        dialogRef.afterClosed().subscribe(
+            data => {
+                if (!data) {
+                    console.log("closed");
+                    return;
+                }
+
+                console.log("userid: "  + data["form"].UserID);
+                
+                var formData = new FormData();
+                if (data["form"].FileName) {
+                    formData.append("FileName", data["form"].FileName);
+                }
+                if (data["form"].isUserInputName) {
+                    formData.append("isUserInputName", data["form"].isUserInputName);
+                } 
+                else {
+                    formData.append("isUserInputName", "false");
+                }
+                formData.append("UserID", "2");
+                formData.append("File", data["file"], data["file"].name);
+                formData.append("DestFolder", path);
+
+                this.submitFormData(formData);
+            });
+    }
+
+    submitFormData(formData : FormData) {
+
+        this.http.post<FileUpload>(this.baseUrl + 'api/FileUpload/UploadFileForm', formData ).subscribe(status => {
+            if (status['result']) {
+                console.log("jeeej");
+            }
+            else {
+                if (status["error_code"] == 421) {
+                    console.log("file with that name already exists");
+                }
+                else {
+                    console.log("form validation failed");
+                }
+            }
+            // window.location.reload();
+            this.getConentsOfFolder(formData.get("DestFolder").toString());
+        });
+    }
 }
 
 class User {
@@ -92,3 +153,10 @@ class Folder {
     subfolders: Map<string, string>;
     name: string;
 }
+
+interface FileUpload {
+    UserID: number,
+    FileName: string,
+    FileObj: File,
+    isUserInputName: boolean
+  }
