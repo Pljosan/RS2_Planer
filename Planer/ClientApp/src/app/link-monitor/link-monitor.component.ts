efcf20dcf1783788fa41ef997ca5abb1cb6d1433
Pljosan <ana.bajic13@gmail.com>
2019-01-19 23:13:40 +0100

    Link monitor v3

--- Planer/ClientApp/src/app/link-monitor/link-monitor.component.ts
+++ Planer/ClientApp/src/app/link-monitor/link-monitor.component.ts
@@ -14,12 +14,15 @@ export class LinkMonitorComponent implements OnInit {
 
     public additions : Array<string>;
     private timer;
-    public notificationClicked : boolean;
+    public notificationClicked : Map<string, Boolean>;
+    public changesPerLink : Map<string, Array<string>>;
+    public objectKeys = Object.keys;
 
     constructor (private dialog: MatDialog, private http: HttpClient, @Inject('BASE_URL') baseUrl: string) { 
         this.baseUrl = baseUrl;
-        this.notificationClicked = true;
         this.additions = new Array<string>();
+        this.changesPerLink = new Map<string, Array<string>>();
+        this.notificationClicked = new Map<string, Boolean>();
 
         this.getChanges();
     }
@@ -35,27 +38,32 @@ export class LinkMonitorComponent implements OnInit {
         this.timer.unsubscribe();
     }
 
-    checkOffNotification() {
-        this.notificationClicked = true;
+    checkOffNotification(link : string) {
+        this.notificationClicked.set(link, true);
         this.additions.length = 0;
     }
 
+    isClicked(link : string) {
+        if (this.notificationClicked.get(link)) {
+            return true;
+        }
+        return false;
+    }
+
     getChanges() {
-        this.http.get(this.baseUrl + 'api/LinkMonitor/getModifyDate').subscribe(result => {
+        this.http.get<Map<string, Object>>(this.baseUrl + 'api/LinkMonitor/getModifyDate').subscribe(result => {
             console.log(result);
-            // result = result.toString
-            if (result["additions"].length > 0) {
-                this.additionsDiv.nativeElement.innerHTML = "";
+            this.changesPerLink = result["additions"];
+            Object.keys(this.changesPerLink).forEach(key => {
 
-                result["additions"].forEach(element => {
-                    this.additions.push(element);
-                })
-                this.additions.forEach(element => {
-                    this.additionsDiv.nativeElement.innerHTML += element;    
-                });
+                console.log("NOTIFICATION: " + this.changesPerLink[key].length);
 
-                this.notificationClicked = false;
-            }
+                if (this.changesPerLink[key].length > 0)
+                    this.notificationClicked.set(key, false);     
+                else if (this.changesPerLink[key].length == 0)
+                    this.notificationClicked.set(key, true);                
+            });
+
           }, error => console.error(error));
     }
 
--- Planer/Controllers/LinkMonitorController.cs
+++ Planer/Controllers/LinkMonitorController.cs
@@ -22,6 +22,8 @@ namespace Planer.Controllers {
 
         private ILinkRepository linkRepository;
         private IUserRepository userRepository;
+        private Dictionary<string, List<string>> changesPerUrl = new Dictionary<string, List<string>>();
+        private List<string> additions = new List<string>();
 
         public LinkMonitorController(ILinkRepository repository, IUserRepository userRepository)
         {
@@ -34,48 +36,12 @@ namespace Planer.Controllers {
 
             Dictionary<string, object> res = new Dictionary<string, object>();
 
-            using (HttpClient client = new HttpClient()) {
-                const string url = "http://poincare.matf.bg.ac.rs/~smalkov/";
-                StringBuilder sb = new StringBuilder();
-                List<string> additions = new List<string>();
-                List<string> removals = new List<string>(); 
-                List<string> changes = new List<string>(); 
-
-                using (HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead)) {
                 
+            var tasks = linkRepository.Links.Select(l => getUrlChanges(l));
+            var results = await System.Threading.Tasks.Task.WhenAll(tasks);
 
-                        string filePath = "../../user1/smalkov.html";
-                        if (System.IO.File.Exists(filePath)) {
-                            
-                            string newText = await response.Content.ReadAsStringAsync();
-                            string oldText = await System.IO.File.ReadAllTextAsync(filePath);
-
-                            var d = new Differ();
-                            var builder = new InlineDiffBuilder(d);
-                            var result = builder.BuildDiffModel(oldText, newText);
-
-                            foreach (var line in result.Lines) {
-                                if (line.Type == ChangeType.Inserted) {
-                                    additions.Add(line.Text);
-                                }
-                            }
-                        }
-
-                        Stream resFile = System.IO.File.Open("../../user1/test.html", FileMode.Create);
-                        StreamWriter resFileWriter = new StreamWriter(resFile);
-                        resFileWriter.Write(sb.ToString());
-                        resFileWriter.Close();
-
-                        using (Stream streamToWriteTo = System.IO.File.Open(filePath, FileMode.Create))
-                        {
-                            Stream webStream = await response.Content.ReadAsStreamAsync();
-                            await webStream.CopyToAsync(streamToWriteTo);
-                        }
-                    
-                }
-                res.Add("result", true);
-                res.Add("additions", additions);
-            }
+            res.Add("result", true);
+            res.Add("additions", changesPerUrl);
 
             return Json(res);
         }
@@ -89,6 +55,48 @@ namespace Planer.Controllers {
             linkRepository.Save(link);
             return Json("jej");
 
+        }
+
+
+        private async Task<Boolean> getUrlChanges(Link link) {
+            
+
+            using (HttpClient client = new HttpClient()) {
+            
+                using (HttpResponseMessage response = await client.GetAsync(link.Url, HttpCompletionOption.ResponseHeadersRead)) {
+            
+                    if (System.IO.File.Exists(link.PathToFile)) {
+                        
+                        string newText = await response.Content.ReadAsStringAsync();
+                        string oldText = await System.IO.File.ReadAllTextAsync(link.PathToFile);
+
+                        var d = new Differ();
+                        var builder = new InlineDiffBuilder(d);
+                        var result = builder.BuildDiffModel(oldText, newText);
+
+                        foreach (var line in result.Lines) {
+                            if (line.Type == ChangeType.Inserted) {
+                                additions.Add(line.Text);
+                            }
+                        }
+                    }
+
+                    using (Stream streamToWriteTo = System.IO.File.Open(link.PathToFile, FileMode.Create))
+                    {
+                        Stream webStream = await response.Content.ReadAsStreamAsync();
+                        await webStream.CopyToAsync(streamToWriteTo);
+                    }
+
+                    List<string> res = new List<string>();
+                    foreach (var addition in additions) {
+                        res.Add(addition);
+                    }
+                    changesPerUrl.Add(link.Url, res);
+                    additions.Clear();
+                }
+            }
+
+            return true;
         }
     }
 }
