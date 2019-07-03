6b822b2f096fa96ddfb2b76e3d34c650c02ebc6e
Pljosan <ana.bajic13@gmail.com>
2019-07-03 20:57:14 +0200

    Updated profile page

--- Planer/ClientApp/src/app/users/users.component.ts
+++ Planer/ClientApp/src/app/users/users.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogConfig } from "@angular/material";
 import { EncrDecrService } from '../encr-decr/encr-decr-service.service';
 import { HttpClient } from '@angular/common/http';
+import { Task } from '../calendar/task.model';
 
 @Component({
   selector: 'app-users',
@@ -12,17 +13,30 @@ export class UsersComponent implements OnInit {
 
   private loggedUserId : number;
   private baseUrl : string;
-  private user : User;
+  public user : User;
+  public upcomingTasks : Array<Task>;
+  public displayedColumns: string[] = ['name', 'date', 'time'];
+  public displayedColumnsUser: string[] = ['firstName', 'lastName', 'email'];
+  public userData: Array<User>;
 
   constructor(private dialog: MatDialog, private http: HttpClient, @Inject('BASE_URL') baseUrl: string, private EncrDecr: EncrDecrService) { 
     this.loggedUserId = parseInt(this.EncrDecr.get('123456$#@$^@1ERF', sessionStorage.getItem('id')));
     this.baseUrl = baseUrl;
+    this.upcomingTasks = new Array<Task>();
+    this.userData = new Array<User>();
   }
 
   ngOnInit() {
     this.http.get<User>(this.baseUrl + 'api/User/GetUserInfo/' + this.loggedUserId).subscribe(result => {
       console.log(result);
         this.user = result;
+        this.userData.push(this.user);
+      },
+    error => console.error(error));
+
+    this.http.get<Array<Task>>(this.baseUrl + 'api/Task/GetUpcomingTasks/' + this.loggedUserId).subscribe(result => {
+      console.log(result);
+        this.upcomingTasks = result;
       },
     error => console.error(error));
   }
